a9c8a3ce1497b9c6f97a34e4a48140724674b84c
Sindhu-Chinnaswamy31 <sindhucswamy@email.com>
2025-06-25 21:42:56 +0530

    swiggy image issue

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,16 +6,29 @@ const Body = () => {
     //Local State variable - super powerfull variable  
     //When ever the state variable changes, the react component will re-render  
     const [listOfRestaurants, setListOfRestaurants] = useState(restaurantList);
+    const [searchQuery, setSearchQuery] = useState('');
 
     return <div className="body">
-        <div className="res-search">
-            <button className="res-filter-btn" 
-            onClick={() => {
-                const filteredRestaurants = listOfRestaurants.filter(
-                  (res) => res.data.avgRating > 4
-                );
-                setListOfRestaurants(filteredRestaurants);//updating the state
-            }}>Top Rated Restaurants</button>
+        <div className="res-filter-search">
+            <div className="res-filter">
+                <button className="res-filter-btn" 
+                onClick={() => {
+                    const filteredRestaurants = listOfRestaurants.filter(
+                    (res) => res.data.avgRating > 4
+                    );
+                    setListOfRestaurants(filteredRestaurants);//updating the state
+                }}>Top Rated Restaurants</button>
+            </div>
+            <div classname="res-filter">
+                <input type="text" className="search-input" placeholder="Search" value={searchQuery} onChange={(event) => {
+                    const query = event.target.value;
+                    setSearchQuery(query);
+                    const filteredRestaurants = restaurantList.filter((res) =>
+                        res.data.name.toLowerCase().includes(query.toLowerCase())
+                    );
+                    setListOfRestaurants(filteredRestaurants);
+                }}/>
+            </div>
         </div>
         <div className="res-container">
             {
--- src/components/RestroCard.js
+++ src/components/RestroCard.js
@@ -5,8 +5,7 @@ const RestroCard = (props) => {
     const {resData} = props;
 
     return (<div className="res-card">
-        {/* <img className="res-logo" src={CDN_URL+resData.data.cloudinaryImageId}/> */}
-        <img className="res-logo-image" src="https://b.zmtcdn.com/data/pictures/0/18567490/80d708a15629cb73aaae69ab89b1e360.jpg?output-format=webp&fit=around%7C771.75:416.25&crop=771.75:416.25;*,*"/>
+        {<img className="res-logo-image" src={CDN_URL+resData.data.cloudinaryImageId}/>}
         <h3 className="res-name">{resData.data.name}</h3>
         <h4 className="res-cuisine">{resData.data.cuisines.join(", ")}</h4>
         <h4 className="res-delivery">₹{resData.data.costForTwo/100} FOR TWO</h4>
