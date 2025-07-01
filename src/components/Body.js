716096d18edfd0a878bc507e8645d9de3e2953a9
Sindhu-Chinnaswamy31 <sindhucswamy@email.com>
2025-07-01 08:59:57 +0530

    useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,44 @@
 import RestaurantCard from "./RestroCard";
-import {useState} from "react";
-import {restaurantList} from "../utils/mockData";
+import {useState, useEffect} from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
     //React hook normal javascript function, it is a utility function it give us some utility.
     //Local State variable - super powerfull variable  
     //When ever the state variable changes, the react component will re-render  
-    const [listOfRestaurants, setListOfRestaurants] = useState(restaurantList);
+    // const [listOfRestaurants, setListOfRestaurants] = useState(restaurantList);
     const [searchQuery, setSearchQuery] = useState('');
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+
+    // synatx of useEffect hook
+    // useEffect(() => {
+    //     console.log("useEffect called")}, 
+    // []);
+    // arraow(call back function), dependency array are two params pass to useEffect hook
+    //callback function will after the initial render of the component and also on re-renders if there is a change in the dependency array
+    //useEffect hook will run only once after the initial render of the component and also on re-renders if there is a change in the dependency array
+    useEffect(()=>{
+        fetchData();
+    }, [])
+
+    const fetchData = async() => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+        ); // give us from js engine, Fetch will retrun promise
+        const json = await data.json();
+        // console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+        //optional chaning
+        const resData = json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        const filteredRestaurants = resData.filter(
+           (res) => res.info
+        );
+       setListOfRestaurants(filteredRestaurants);
+    }
+
+    //conditional rendering : rendering based on condition
+    if(listOfRestaurants.length === 0){
+        return <Shimmer/>
+    }
 
     return <div className="body">
         <div className="res-filter-search">
@@ -14,17 +46,17 @@ const Body = () => {
                 <button className="res-filter-btn" 
                 onClick={() => {
                     const filteredRestaurants = listOfRestaurants.filter(
-                    (res) => res.data.avgRating > 4
+                    (res) => res.info.avgRating > 4.5
                     );
-                    setListOfRestaurants(filteredRestaurants);//updating the state
+                   setListOfRestaurants(filteredRestaurants);//updating the state
                 }}>Top Rated Restaurants</button>
             </div>
-            <div classname="res-filter">
+            <div className="res-filter">
                 <input type="text" className="search-input" placeholder="Search" value={searchQuery} onChange={(event) => {
                     const query = event.target.value;
                     setSearchQuery(query);
-                    const filteredRestaurants = restaurantList.filter((res) =>
-                        res.data.name.toLowerCase().includes(query.toLowerCase())
+                    const filteredRestaurants = listOfRestaurants.filter((res) =>
+                        res.info.name.toLowerCase().includes(query.toLowerCase())
                     );
                     setListOfRestaurants(filteredRestaurants);
                 }}/>
--- src/components/RestroCard.js
+++ src/components/RestroCard.js
@@ -3,14 +3,15 @@ import {CDN_URL} from "../utils/contants"
 //3. Restaurant Card
 const RestroCard = (props) => {
     const {resData} = props;
-
+    const data = resData.data || resData.info || {};
+    console.log(data)
     return (<div className="res-card">
-        {<img className="res-logo-image" src={CDN_URL+resData.data.cloudinaryImageId}/>}
-        <h3 className="res-name">{resData.data.name}</h3>
-        <h4 className="res-cuisine">{resData.data.cuisines.join(", ")}</h4>
-        <h4 className="res-delivery">₹{resData.data.costForTwo/100} FOR TWO</h4>
-        <h4 className="res-deliveryTime">{resData.data.deliveryTime} mins</h4>
-        <h4 className="res-rating">{resData.data.avgRating} ⭐</h4>
+        {<img className="res-logo-image" src={CDN_URL+data.cloudinaryImageId}/>}
+        <h3 className="res-name">{data.name}</h3>
+        <h4 className="res-cuisine">{data.cuisines.join(", ")}</h4>
+        <h4 className="res-delivery">{data.costForTwo}</h4>
+        <h4 className="res-deliveryTime">{data.deliveryTime} mins</h4>
+        <h4 className="res-rating">{data.avgRating} ⭐</h4>
     </div>);
 }
 
